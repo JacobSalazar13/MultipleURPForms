fea0ce5664948adb8d18e46b3b6c9d199297e675
marleyrosario <70246062+marleyrosario@users.noreply.github.com>
2023-06-06 19:29:06 -0500

    requests

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-
+import requests 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -8,4 +8,5 @@ def form():
         return render_template('Form.html')
     else:
         form_data = request.form.to_dict()
+        response = requests.post("https://hooks.zapier.com/hooks/catch/6860943/3tpp32p/", json = form_data)
         return jsonify(form_data), 200
