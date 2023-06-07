7d8e808b1c86c0f726ee7adfb3de7ee5dd0a050a
marleyrosario <70246062+marleyrosario@users.noreply.github.com>
2023-06-07 09:57:02 -0500

    add js

--- static/Form.js
+++ static/Form.js
@@ -3,11 +3,25 @@ function showDropDownQuestions(event) {
     var dropdown = $('#' + dropdownId);
     dropdown.toggle();
 
-    // If dropdown is visible, add required attribute to inputs.
+    // If dropdown is visible, add hidden input with label text.
     // If not, remove it.
     if (dropdown.is(':visible')) {
         dropdown.find('input').attr('required', true);
+
+        // Create hidden input with label text
+        var labelText = $('label[for="' + dropdownId + '"]').text();
+        var hiddenInput = $('<input>').attr({
+            type: 'hidden',
+            name: dropdownId + '_label',
+            value: labelText
+        });
+        
+        // Append hidden input to form
+        $('form').append(hiddenInput);
     } else {
         dropdown.find('input').removeAttr('required');
+        
+        // Remove hidden input
+        $('input[name="' + dropdownId + '_label"]').remove();
     }
 }
