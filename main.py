e58b0f59d73960c17961355bf997936a7fa7dc82
marleyrosario <70246062+marleyrosario@users.noreply.github.com>
2023-06-07 09:36:30 -0500

    nocache

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response
 import requests 
+import functools
+
 app = Flask(__name__)
 
+def nocache(view):
+    @functools.wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+    return no_cache
+
 @app.route('/', methods = ['GET', 'POST'])
+@nocache
 def form():
     if request.method == 'GET':
         return render_template('Form.html')
