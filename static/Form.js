7a76f50326fd63827969ba737bca3ad65e91b47e
marleyrosario <70246062+marleyrosario@users.noreply.github.com>
2023-06-07 11:09:22 -0500

    add interactivity

--- static/Form.js
+++ static/Form.js
@@ -3,17 +3,19 @@ function showDropDownQuestions(event) {
     var dropdown = $('#' + dropdownId);
     dropdown.toggle();
 
-    // If dropdown is visible, add hidden input with label text.
+    // If dropdown is visible, add hidden input with the value of another input.
     // If not, remove it.
     if (dropdown.is(':visible')) {
         dropdown.find('input').attr('required', true);
 
-        // Create hidden input with label text
-        var labelText = $('label[for="' + dropdownId + '"]').text();
+        // Get value of the other input field
+        var inputValue = dropdown.find('input[name="hiddenfield"]').val(); // Replace 'your_input_name' with the name of the input you want to fetch the value from.
+
+        // Create hidden input with the fetched value
         var hiddenInput = $('<input>').attr({
             type: 'hidden',
-            name: dropdownId + '_label',
-            value: labelText
+            name: dropdownId + '_hidden',
+            value: inputValue
         });
         
         // Append hidden input to form
@@ -22,6 +24,6 @@ function showDropDownQuestions(event) {
         dropdown.find('input').removeAttr('required');
         
         // Remove hidden input
-        $('input[name="' + dropdownId + '_label"]').remove();
+        $('input[name="' + dropdownId + '_hidden"]').remove();
     }
 }
