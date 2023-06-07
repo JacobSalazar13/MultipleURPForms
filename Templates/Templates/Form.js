e2b675cac53d42e55733548389b5817c6b62547a
Myles <mrosar6@uic.edu>
2023-06-07 14:50:58 -0500

    updatew

--- Templates/Templates/Form.js
+++ Templates/Templates/Form.js
@@ -1,11 +1,29 @@
 function showDropDownQuestions(event) {
-    var button = event.target;
-    var dropdownId = button.getAttribute('data-dropdown');
+    var dropdownId = $(event.target).data('dropdown');
+    var dropdown = $('#' + dropdownId);
+    dropdown.toggle();
 
-    var dropdown = document.getElementById(dropdownId);
-    if (dropdown.style.display === 'none') {
-        dropdown.style.display = 'block';
+    // If dropdown is visible, add hidden input with the value of another input.
+    // If not, remove it.
+    if (dropdown.is(':visible')) {
+        dropdown.find('input').attr('required', true);
+
+        // Get value of the other input field
+        var inputValue = dropdown.find('input[name="hiddenfield"]').val(); // Replace 'your_input_name' with the name of the input you want to fetch the value from.
+
+        // Create hidden input with the fetched value
+        var hiddenInput = $('<input>').attr({
+            type: 'hidden',
+            name: dropdownId + '_hidden',
+            value: inputValue
+        });
+        
+        // Append hidden input to form
+        $('form').append(hiddenInput);
     } else {
-        dropdown.style.display = 'none';
+        dropdown.find('input').removeAttr('required');
+        
+        // Remove hidden input
+        $('input[name="' + dropdownId + '_hidden"]').remove();
     }
 }
