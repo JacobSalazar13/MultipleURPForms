7ceb416cde45fc1f28ef6ac12af5ed30395816e8
marleyrosario <70246062+marleyrosario@users.noreply.github.com>
2023-06-30 07:25:08 -0500

    push changes

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ from google.cloud import firestore
 
 app = Flask(__name__)
 
+def extract_emails_and_names(data):
+    emails = []
+    names = []
+
+    for course_data in data[0].values():
+        if any(value != "" for value in course_data.values()):
+            if course_data.get('email', '') != "":
+                emails.append(course_data['email'])
+            if course_data.get('name', '') != "":
+                names.append(course_data['name'])
+
+    return emails, names
 
 def log(message, client):
     try:
@@ -74,6 +86,8 @@ def form():
             pass
         form_data = request.form.to_dict()
         session_id=generate_session_id(5)
+        if 'payment' not in list(form_data.keys()):
+            return render_template("Form.html", error = "Please choose a payment method!")
         print(form_data)
         print("got data")
         # process the raw_data to get the format you want
@@ -150,6 +164,8 @@ def form():
         form_data["processed_data"] = processed_data
         print(form_data)  # Prints the processed data to the console
         quantities_list = [int(v.get('quantity')) for v in list(processed_data[0].values()) if len(v.get('quantity')) > 0]
+        if len(quantities_list) == 0:
+            return render_template("Form.html", error = "Please place an order!")
         for quantity in quantities_list:
             if quantity < 10:
                 return render_template("Form.html", error = "Unfortunately, you cannot order an Ultimate Review Packet for less than 10 licenses. Please increase your order size.")
@@ -184,7 +200,6 @@ def form():
         form_data['workSheetCodes'] = workSheetCodes
         form_data['sampleCodes'] = sampleCodes
         form_data['product_ids_list'] = product_ids_list
-        print(form_data)
         form_data['ID'] = session_id
         try:
             form_data["timestamp"] = time.time()
@@ -216,6 +231,14 @@ def form():
             doc_ref.set(form_data)
         except:
             pass
+        try:
+            emails_list = [str(v.get('email')) for v in list(processed_data[0].values()) if len(v.get('email')) > 0]
+            names_list = [str(v.get('name')) for v in list(processed_data[0].values()) if len(v.get('name')) > 0]
+            form_data['emails_list'] = emails_list
+            form_data['names_list'] = names_list
+            print(form_data)
+        except Exception as e:
+            print("error" + str(e))
         return redirect(url_for("route_success", order_id=session_id))
 
 @app.route("/success/<order_id>")
